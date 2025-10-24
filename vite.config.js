9f93bb37120b3192aa1a6967288e3db93687e65c
Furkan Çakır <62892586+AkzoCodie@users.noreply.github.com>
2025-10-25 02:43:01 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// @vitejs/plugin-react devDependency’sini eklemeden de çalışır (fast refresh olmadan).
-// İstersen ekleyebilirsin; şimdilik vanilla kalsın:
-export default defineConfig({
-  plugins: [],
-})
